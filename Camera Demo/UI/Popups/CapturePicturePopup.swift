171f54cb988e936922d0e7b8d63780bec80a0738
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-17 00:46:29 +0100

    1

--- Camera Demo/UI/Popups/CapturePicturePopup.swift
+++ Camera Demo/UI/Popups/CapturePicturePopup.swift
@@ -57,11 +57,3 @@ private extension CapturePicturePopup {
 private extension CapturePicturePopup {
 
 }
-
-
-
-extension Task where Success == Never, Failure == Never {
-    static func sleep(seconds: CGFloat) async {
-        try! await Task.sleep(nanoseconds: UInt64(seconds * 1_000_000_000))
-    }
-}
